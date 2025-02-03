d91ea4cbc051dca46feef6232fc4ea5615a830e0
Ksym Owly <35627613+OksanaGorobey@users.noreply.github.com>
2025-02-03 10:29:46 +0200

    add edit event UpdateAdressesOverdueExcel.js

--- UpdateAdressesOverdueExcel.js
+++ UpdateAdressesOverdueExcel.js
@@ -25,17 +25,49 @@ class UpdateAdressesOverdue {
 
     //беремо одразу другу строку заголовків
     this.headers = this.data[1];
+  }
 
-    // очищуємо сторінку від попередніх значень
+  _clearSheetPrevious() {
     this.sheet.getRange("A3:Z500").setBackground(null);
   }
 
-  highlightOverdue() {
+  _clearRowPrevious(row) {
+    this.sheet.getRange(`A${row}:Z${row}`).setBackground(null);
+  }
+
+  highlightOverdueOnEdit(e) {
+    //отримуємо діапазон зміненої комірки та номер 
+    let range = e.range; 
+    let row = range.getRow(); 
+    
+    if(row > 2) {
+      // очищуємо строку від попередніх значень
+      this._clearRowPrevious(row);
+
+      // Отримуємо всю строку, в якій відбулося редагування
+      let line = this.sheet.getRange(row, 1, 1, this.sheet.getLastColumn()).getValues()[0];
+
+      //встановлюємо де є 'Консьерж' світло жовтий колір
+      this.setBackgroundConcierge(row, line);
+
+      //ініціюємо змінні для останніх встановлених дат
+      this.setLastDates(line);
+
+      //перевіряємо чи є "просрочка" по датам і якщо так змінюємо колір потрібній строчці чи комірці
+      this.isOverdueLastDates(row)
+    }
+  }
+
+  highlightOverdueDaily() {
+
+    // очищуємо сторінку від попередніх значень
+    this._clearSheetPrevious();
+
     if(this.data.length < 1 ){
       return;
     }
 
-    for (let row = 2; row < this.data.length; row++) {
+    for (let row = 2; row < this.data.length;) {
       let line = this.data[row];
 
       //очищуємо попередні данні
@@ -43,13 +75,21 @@ class UpdateAdressesOverdue {
       this.lastPassen = '';
       this.lastTakenCol = 0;
 
+      //одразу встановлюємо вірний номер рядка
+      row++;
+
+      //встановлюємо усім строчкам що є пустими або розділювачами між територією чорний колір
+      if(line.every(element => element == '')) {
+          this.setPartDelimeter(row);
+      }
+
       //встановлюємо усім строчкам де є 'Консьерж' світло жовтий колір
       this.setBackgroundConcierge(row, line);
 
-      //ініціюємо останні змінні для останніх встановлених дат
+      //ініціюємо змінні для останніх встановлених дат
       this.setLastDates(line);
 
-      //перевіряємо чи є "просрочка" по датам і якщо так змінюємо колір потрібній строчці чи ячейці
+      //перевіряємо чи є "просрочка" по датам і якщо так змінюємо колір потрібній строчці чи комірці
       if (!this.isOverdueLastDates(row)) {
           continue;
       }
@@ -58,8 +98,12 @@ class UpdateAdressesOverdue {
 
   setBackgroundConcierge(row, line) {
     if(line.includes('Консьерж')) {
-      this.sheet.getRange(row + 1, 1, 1, this.headers.length).setBackground("lightgoldenrodyellow");
+      this.sheet.getRange(row, 1, 1, this.headers.length).setBackground('#f5b041');
     }
+  }
+
+  setPartDelimeter(row) {
+    this.sheet.getRange(row, 1, 1, this.headers.length).setBackground('black');
   }
 
   setLastDates(line) {
@@ -80,7 +124,7 @@ class UpdateAdressesOverdue {
     }      
   }
 
-  isOverdueLastDates(row) {
+  isOverdueLastDates(row) {      
     if (this.lastTaken == '' && this.lastPassen == '') {
       return false;
     }
@@ -89,14 +133,16 @@ class UpdateAdressesOverdue {
       let diffDays = ((this.today - this.lastPassen) / this.diffRound);
 
       if ((this.lastTaken == '' || this.lastPassen > this.lastTaken) && (diffDays > this.overdueDays)) { 
-        this.sheet.getRange(row + 1, 1, 1, this.headers.length).setBackground("yellowgreen");
+        this.sheet.getRange(row, 1, 1, this.headers.length).setBackground('#58d68d ');
       }
     } else {
 
       let diffDays = ((this.today - this.lastTaken) / this.diffRound);
 
       if (this.lastTaken != '' && (diffDays > this.overdueDays)) {
-        this.sheet.getRange(row + 1, this.lastTakenCol + 2).setBackground("crimson");
+        this.sheet.getRange(row, this.lastTakenCol + 2).setBackground('#ec7063');
+        this.sheet.getRange(row, 2).setBackground('#ec7063');
+
       }
     }
 
@@ -104,8 +150,3 @@ class UpdateAdressesOverdue {
   }
 
 }
-
-
-
-
-
